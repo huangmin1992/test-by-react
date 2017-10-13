1c9916ad8433f7a147032038fb37ac1894775808
huangmin1992 <854982095@qq.com>
2017-10-13 11:49:45 +0800

    two

--- test-app/src/component/imgstate.js
+++ test-app/src/component/imgstate.js
@@ -1,6 +1,23 @@
 import React,{Component} from 'react';
 import imagesData from '../imagesData/imagesData.json'
 
+//图片详情
+class ImgList extends Component{
+	render(){
+		const {imgsList} = this.props;
+		return(
+			<figure className='img-figure'>
+				<img src={imgsList.fileName}/>
+				<figcaption>
+					<h2 className="img-title">{imgsList.title}</h2>
+				</figcaption>
+			</figure>
+		)
+	}
+}
+
+
+
 class ImgState extends Component{
 	render(){
 		return(
@@ -8,7 +25,7 @@ class ImgState extends Component{
 				<section className="img-list">
 					{
 						imagesData.map((imgs,index)=>
-				            (<img key={index} src={imgs.fileName} />)
+				            (<ImgList key={index} imgsList = {imgs} />)
 				        )
 					}
 				</section>
